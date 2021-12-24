2301e7045e854a59c4491dae86213bafb69b48b8
Dylan Socolobsky <dsocolobsky@gmail.com>
2021-12-24 01:06:08 -0300

    idents, numbers, strings (problem when no spaces)

--- lib/vvdc/lexer.rb
+++ lib/vvdc/lexer.rb
@@ -1,8 +1,38 @@
 module Vvdc
 
+  class Token
+    def initialize(type, literal)
+      @type = type
+      @literal = literal
+    end
+
+    attr_reader :type
+    attr_reader :literal
+  end
+
   class Lexer
     def initialize
       @tokens = []
+    end
+
+    def add(type, symbol)
+      @tokens << Token.new(type, symbol)
+    end
+
+    def add_symbol(literal)
+      self.add(:symbol, literal)
+    end
+
+    def digit?(ch)
+      48 <= ch.ord and ch.ord <= 57
+    end
+
+    def alphabetic?(ch)
+      (65 <= ch.ord and ch.ord <= 90) or (97 <= ch.ord and ch.ord <= 122)
+    end
+
+    def identifier_char?(ch)
+      self.alphabetic?(ch) or self.digit?(ch) or ch == "_"
     end
 
     def scan(program)
@@ -10,54 +40,78 @@ module Vvdc
       chars = program.split('')
       while idx < chars.length
         case chars[idx]
+        when " "
+          nil
         when "+"
-          @tokens << "+"
+          self.add_symbol("+")
         when "-"
-          @tokens << "-"
+          self.add_symbol("-")
         when "*"
-          @tokens << "*"
+          self.add_symbol("*")
         when ";"
-          @tokens << ";"
+          self.add_symbol(";")
         when "("
-          @tokens << "("
+          self.add_symbol("(")
         when ")"
-          @tokens << ")"
+          self.add_symbol(")")
         when "{"
-          @tokens << "{"
+          self.add_symbol("{")
         when "}"
-          @tokens << "}"
-        when ")"
-          @tokens << ")"
+          self.add_symbol("}")
         when "="
           if idx + 1 < program.length and chars[idx+1] == "="
-            @tokens << "=="
+            self.add_symbol("==")
             idx = idx + 1
           else
-            @tokens << "="
+            self.add_symbol("=")
           end
         when "!"
           if idx + 1 < program.length and chars[idx+1] == "="
-            @tokens << "!="
+            self.add_symbol("!=")
             idx = idx + 1
           else
-            @tokens << "!"
+            self.add_symbol("!")
           end
         when "<"
           if idx + 1 < program.length and chars[idx+1] == "="
-            @tokens << "<="
+            self.add_symbol("<=")
             idx = idx + 1
           else
-            @tokens << "<"
+            self.add_symbol("<")
           end
         when ">"
           if idx + 1 < program.length and chars[idx+1] == "="
-            @tokens << ">="
+            self.add_symbol(">=")
             idx = idx + 1
           else
-            @tokens << ">"
+            self.add_symbol(">")
           end
         else
-          nil
+          if chars[idx] == "\""
+            st = ""
+            idx = idx + 1
+            while idx < program.length and chars[idx] != "\""
+              st << program[idx]
+              idx = idx + 1
+            end
+            self.add(:string, st)
+          elsif self.digit?(chars[idx])
+            numb = chars[idx]
+            idx = idx + 1
+            while idx < program.length and self.digit?(chars[idx])
+              numb << program[idx]
+              idx = idx + 1
+            end
+            self.add(:number, numb)
+          else  # Must be an identifier
+            ident = chars[idx]
+            idx = idx + 1
+            while idx < program.length and self.identifier_char?(chars[idx])
+              ident << program[idx]
+              idx = idx + 1
+            end
+            self.add(:identifier, ident)
+          end
         end
         idx = idx + 1
       end
--- test/parser_test.rb
+++ test/parser_test.rb
@@ -9,7 +9,7 @@ class LexerTest < Minitest::Test
 
     tokens = lexer.scan(program)
 
-    assert_equal program.delete(' ').split(''), tokens
+    assert_equal program.delete(' ').split(''), tokens.map { |token| token.literal }
   end
 
   def test_simple_tokens_no_spaces
@@ -18,7 +18,7 @@ class LexerTest < Minitest::Test
 
     tokens = lexer.scan(program)
 
-    assert_equal program.split(''), tokens
+    assert_equal program.split(''), tokens.map { |token| token.literal }
   end
 
   def test_combined_tokens
@@ -27,7 +27,7 @@ class LexerTest < Minitest::Test
 
     tokens = lexer.scan(program)
 
-    assert_equal program.split(' '), tokens
+    assert_equal program.split(' '), tokens.map { |token| token.literal }
   end
 
   def test_combined_tokens_no_spaces
@@ -36,6 +36,26 @@ class LexerTest < Minitest::Test
 
     tokens = lexer.scan(program)
 
-    assert_equal ["==", "=", "!=", "<=", ">="], tokens
+    assert_equal ["==", "=", "!=", "<=", ">="], tokens.map { |token| token.literal }
+  end
+
+  def test_numbers_identifiers_and_strings
+    lexer = Vvdc::Lexer.new
+    program = "1337 identifier otheridentifier \"string with spaces\""
+
+    tokens = lexer.scan(program)
+
+    assert_equal ["1337", "identifier", "otheridentifier", "string with spaces"], tokens.map { |token| token.literal }
+    assert_equal [:number, :identifier, :identifier, :string], tokens.map { |token| token.type }
+  end
+
+  def test_numbers_identifiers_mixed_with_symbols
+    lexer = Vvdc::Lexer.new
+    program = "42<16==banana5 32"
+
+    tokens = lexer.scan(program)
+
+    assert_equal ["42", "<", "16", "==", "banana5", "32"], tokens.map { |token| token.literal }
+    assert_equal [:number, :symbol, :number, :symbol, :identifier, :number], tokens.map { |token| token.type }
   end
 end
